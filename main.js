5c5f157b52108f94c4c10c8d69c886ddf43e1a56
mukundcode <83866313+mukundcode@users.noreply.github.com>
2021-05-25 15:53:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculate()
+{
+ area=document.getElementById("btn1").value*document.getElementById("btn1").value;
+ perimeter=document.getElementById("btn1").value*4;
+ document.getElementById("lblarea").innerHTML=area;
+ document.getElementById("lblperimeter").innerHTML=perimeter;
+}
